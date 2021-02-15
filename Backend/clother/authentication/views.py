8685a86ebe3789e101ccf824dc16c95e9428b7b9
Fedor Sokolov <fnsokolov@edu.hse.ru>
2021-02-15 20:16:59 +0300

    Add password recovery on the server side

--- Backend/clother/authentication/views.py
+++ Backend/clother/authentication/views.py
@@ -1,16 +1,17 @@
 import re
 import time
 
-from email_validator import validate_email, EmailNotValidError
-from flask import Blueprint, current_app, request, url_for, render_template
+from flask import Blueprint, current_app, request, url_for, render_template, after_this_request
 from flask_jwt_extended import create_access_token, create_refresh_token
+from email_validator import validate_email, EmailNotValidError
 from itsdangerous import URLSafeTimedSerializer
 from itsdangerous.exc import BadSignature, SignatureExpired, BadData
 from sqlalchemy.exc import IntegrityError
 
 from clother import db
 from clother.user.models import User
-from clother.utils import send_email
+from clother.utils import send_email, validate_password
+from .forms import ResetPasswordForm
 
 blueprint = Blueprint('auth', __name__)
 
@@ -27,12 +28,8 @@ def register():
     except EmailNotValidError:
         return {'message': 'Email address is invalid'}, 422
 
-    # [8; 25] characters, at least 1 digit, at least 1 lowercase letter, at least 1 uppercase letter, no whitespaces,
-    # at least 1 special character
-    password_regex = re.compile(r'^(?=\S{8,25}$)(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=\S+$)(?=.*?[^A-Za-z\s0-9])')
-
     password = data['password']
-    if not password_regex.match(password):
+    if not validate_password(password):
         return {'message': 'Password should be at least 8 and less than 25 characters, '
                            'contain at least 1 digit, 1 uppercase letter, 1 lowercase letter and '
                            '1 special character'}, 422
@@ -50,7 +47,7 @@ def register():
         db.session.commit()
     except IntegrityError:
         db.session.rollback()
-        return {'message': 'User with this email is already exists'}, 403
+        return {'message': 'User with this email address is already exists'}, 403
 
     ts = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
     token = ts.dumps(user.email, salt='confirm_email')
@@ -73,7 +70,7 @@ def register():
             }
 
 
-@blueprint.route('/auth/confirm/<token>')
+@blueprint.route('/auth/confirm_email/<token>')
 def confirm_email(token):
     ts = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
     try:
@@ -118,6 +115,72 @@ def login():
         return {"message": "Wrong email or password"}, 403
 
 
+@blueprint.route('/auth/forgot_password', methods=['POST'])
+def forgot_password():
+    if not request.is_json:
+        return {"message": "Missing JSON in request"}, 400
+    data = request.get_json()
+
+    email = data['email']
+    try:
+        valid_email = validate_email(email).email
+    except EmailNotValidError:
+        return {'message': 'Email address is invalid'}, 422
+
+    user = User.query.filter_by(email=valid_email).first()
+    if not user:
+        return {'message': "User with this email address doesn't exist"}, 422
+
+    ts = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+    token = ts.dumps({'email': user.email, 'password': user.password}, salt='reset_password')
+    confirmation_url = url_for(
+        'auth.reset_password',
+        token=token,
+        _external=True)
+
+    html = render_template(
+        'confirmation.html',
+        subject_text='Password reset',
+        body_text="To reset your password, please press the button below. If you didn't request a password reset, "
+                  "just ignore this email",
+        button_text='Reset password',
+        action_url=confirmation_url)
+
+    send_email(subject='Forgot your password?', recipients=[user.email], html=html)
+
+    return {'message': 'Check your inbox'}
+
+
+@blueprint.route('/auth/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    @after_this_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        return response
+
+    form = ResetPasswordForm()
+
+    ts = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+    try:
+        data = ts.loads(token, salt="reset_password", max_age=900)  # 15 minutes
+    except SignatureExpired:
+        return {'message': "Token has expired"}, 403
+    except BadSignature:
+        return {'message': "Invalid token"}, 403
+
+    user = User.query.filter_by(email=data['email']).first()
+
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        return {'message': 'Password has been successfully changed'}
+
+    if not user.password == data['password']:
+        return {'message': 'Password has already been reset'}, 410
+
+    return render_template('reset_password.html', form=form)
+
+
 @blueprint.route('/email_test')
 def get_email_test():
     html = render_template(
@@ -146,6 +209,12 @@ def get_template_test():
         body_text='To complete email verification, please press the button below.',
         button_text='Verify email',
         action_url='https://google.com')
+
+
+@blueprint.route('/form_test')
+def get_form_test():
+    form = ResetPasswordForm()
+    return render_template('reset_password.html', form=form)
 
 
 @blueprint.route('/')
--- Backend/clother/utils.py
+++ Backend/clother/utils.py
@@ -1,3 +1,4 @@
+import re
 from threading import Thread
 from flask import current_app
 from flask_mail import Message
@@ -12,3 +13,13 @@ def send_email_async(app, message):
 def send_email(subject, recipients, html):
     message = Message(subject=subject, recipients=recipients, html=html)
     return Thread(target=send_email_async, args=(current_app._get_current_object(), message)).start()  # noqa
+
+
+def validate_password(password):
+    return get_password_regex().match(password)
+
+
+# [8; 25] characters, at least 1 digit, at least 1 lowercase letter, at least 1 uppercase letter, no whitespaces,
+# at least 1 special character
+def get_password_regex():
+    return re.compile(r'^(?=\S{8,25}$)(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=\S+$)(?=.*?[^A-Za-z\s0-9])')
