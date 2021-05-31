95b2faf6e02c01696d5eed77993345ec7b759257
Fedor Sokolov <fnsokolov@edu.hse.ru>
2021-05-31 14:05:24 +0300

    Add basic UI tests

--- Client/app/src/androidTest/java/com/t3ddyss/clother/MainTestRunner.kt
+++ Client/app/src/androidTest/java/com/t3ddyss/clother/MainTestRunner.kt
@@ -2,11 +2,20 @@ package com.t3ddyss.clother
 
 import android.app.Application
 import android.content.Context
+import android.os.Bundle
+import android.os.StrictMode
 import androidx.test.runner.AndroidJUnitRunner
 import dagger.hilt.android.testing.HiltTestApplication
 
 class MainTestRunner : AndroidJUnitRunner() {
     override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
         return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+
+    override fun onCreate(arguments: Bundle?) {
+        // NetworkOnMainThread exception when running Espresso tests because of the MockWebServer
+        // https://github.com/square/okhttp/issues/3184
+        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
+        super.onCreate(arguments)
     }
 }
